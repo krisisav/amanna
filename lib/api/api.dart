c112a396385f247e551b3311a63a8534b9260c75
Kristina Savova <kristina.savova13@gmail.com>
2021-06-19 16:32:10 +0100

    Create an API class which will make the calls to the api

---
+++ lib/api/api.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+
+class API {
+  final _dio = Dio();
+  static final baseUrl = 'https://api.quotable.io';
+
+  Future<List<Map<String, dynamic>>?> loadPageOfQuotes(int page) async {
+    assert(page > 0 && page < 95);
+
+    final url = '$baseUrl/quotes?page=$page';
+
+    try {
+      final Response response = await _dio.get(url);
+      print('Successfully loaded quotes from page $page!');
+
+      return response.data['results']!;
+    } on DioError catch (error) {
+      print('${error.type} : ${error.message}');
+    }
+  }
+
+  Future getTags() async {
+    final url = '$baseUrl/tags';
+
+    try {
+      final Response response = await _dio.get(url);
+      print('Successfully loaded tags!');
+
+      return response.data!;
+    } on DioError catch (error) {
+      print('${error.message} : ${error.response}');
+    }
+  }
+}
