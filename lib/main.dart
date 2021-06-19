384e3b54ad27b9f12404c4b86abe071a32e6591b
Kristina Savova <kristina.savova13@gmail.com>
2021-06-19 16:35:00 +0100

    Rebuild main.dart as a list of tag cards and call a getIt set up method before running the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
+import 'package:amanna/api/api.dart';
+import 'package:amanna/database/database.dart';
+import 'package:amanna/models/tags.dart';
+import 'package:amanna/repositories/db_quote_repository.dart';
+import 'package:amanna/repositories/quote_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupGetIt() {
+  getIt.registerSingleton<API>(API());
+  getIt.registerSingleton<DatabaseProvider>(DatabaseProvider());
+  getIt.registerSingleton<QuoteRepository>(DbQuoteRepository());
+  getIt.registerSingletonAsync<List<Tag>>(() async => Tags().tags);
+}
 
 void main() {
+  setupGetIt();
   runApp(MyApp());
 }
 
@@ -12,12 +28,27 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(),
+      home: FutureBuilder<List<Tag>>(
+        future: getIt.getAsync<List<Tag>>(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState != ConnectionState.done) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return MyHomePage(snapshot.data!);
+        }
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  final List<Tag> tags;
+
+  MyHomePage(this.tags);
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -29,7 +60,18 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('AMANNA'),
       ),
-      body: Container(),
+      body: ListView.builder(
+        shrinkWrap: true,
+        physics: BouncingScrollPhysics(
+          parent: AlwaysScrollableScrollPhysics(),
+        ),
+        itemCount: widget.tags.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Text(widget.tags[index].name),
+          );
+        },
+      ),
     );
   }
 }
