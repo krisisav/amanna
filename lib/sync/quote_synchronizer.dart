658eb4163bc753262d690224224870db171ab825
Kristina Savova <kristina.savova13@gmail.com>
2021-06-19 16:33:49 +0100

    Create a QuoteSynchronizer class which will have 2 dependencies: API and QuoteRepository and will get the data from the api and save it to SQLite

---
+++ lib/sync/quote_synchronizer.dart
@@ -0,0 +1,23 @@
+import 'package:amanna/api/api.dart';
+import 'package:amanna/main.dart';
+import 'package:amanna/models/quote.dart';
+import 'package:amanna/repositories/quote_repository.dart';
+
+class QuoteSynchronizer {
+  final _api = getIt.get<API>();
+  final _quoteRepository = getIt.get<QuoteRepository>();
+
+  Future<void> saveQuotesToDatabase(int page) async {
+    final quotesList = await _api.loadPageOfQuotes(page);
+
+    if(quotesList != null && quotesList.isNotEmpty) {
+      quotesList.forEach((mappedQuote) async {
+        final quote = Quote.fromMap(mappedQuote);
+
+        if(await _quoteRepository.contains(quote) == false) {
+          await _quoteRepository.save(Quote.fromMap(mappedQuote));
+        }
+      });
+    }
+  }
+}
